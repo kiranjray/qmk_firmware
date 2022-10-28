41eae8ab7cd001b2b8f8b0bb7bcb8fcbec89faab
kkjray <krishnkiran.ray@gmail.com>
2022-10-28 18:08:45 -0400

    add encoder support, mapping update

--- keyboards/preonic/preonic.c
+++ keyboards/preonic/preonic.c
@@ -11,17 +11,23 @@ const uint8_t music_map[MATRIX_ROWS][MATRIX_COLS] = LAYOUT_preonic_grid(
 #ifdef ENCODER_ENABLE
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
-    if (get_highest_layer(layer_state) > 0) {
+    if (get_highest_layer(layer_state) == 0) {
         if (clockwise) {
             tap_code_delay(KC_VOLU, 10);
         } else {
             tap_code_delay(KC_VOLD, 10);
         }
+    } else if (get_highest_layer(layer_state) == 1){
+        if (clockwise) {
+            tap_code_delay(KC_BRIU, 10);
+        } else {
+            tap_code_delay(KC_BRID, 10);
+        }
     } else {
         if (clockwise) {
-            tap_code_delay(KC_VOLU, 10);
+            tap_code_delay(KC_BRIU, 10);
         } else {
-            tap_code_delay(KC_VOLD, 10);
+            tap_code_delay(KC_BRID, 10);
         }
     }
     return true;
